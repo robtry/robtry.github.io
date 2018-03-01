fd3caccf77e32369f7192631c9389bfb2cb23d4f
Roberto <rgg.correo@gmail.com>
2018-02-28 20:51:04 -0600

    animando el scroll

--- js/main.js
+++ js/main.js
@@ -12,4 +12,20 @@ $(document).ready(function(){
 			bottom: $target.offset().top + $target.outerHeight() - $this.height()
 		});
 	});
+
+	$(".to-home").click(function(){
+		$("html, body").animate({ scrollTop: $('#home').offset().top }, 800);
+	});
+	$(".to-about").click(function(){
+		$("html, body").animate({ scrollTop: $('#about').offset().top }, 800);
+	});
+	$(".to-skills").click(function(){
+		$("html, body").animate({ scrollTop: $('#skills').offset().top }, 800);
+	});
+	$(".to-projects").click(function(){
+		$("html, body").animate({ scrollTop: $('#projects').offset().top }, 800);
+	});
+	$(".to-contact").click(function(){
+		$("html, body").animate({ scrollTop: $('#contact').offset().top }, 800);
+	});
 });
